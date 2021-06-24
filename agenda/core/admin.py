4846f5af13d99a3d86a46605ef4e8bc033b77193
Mateus Duarte <mateusduarte1984@gmail.com>
2021-06-24 13:06:23 -0300

    Criação e formatação tabela

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento')
+    list_filter = ('titulo', 'data_evento',)
+
+# Registrar no admin o evento
+admin.site.register(Evento, EventoAdmin)
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Cria a classe da tabela (inserir no banco)
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+# Cria o nome da tabela
+    class Meta:
+        db_table = 'evento'
+
+    # Altera o nome do evento
+
+    def __str__(self):
+        return self.titulo
+
+
